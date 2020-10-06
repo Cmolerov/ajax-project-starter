b217829aba3ec9af48426d8bfc883f0069155d6f
carlos molero <cmolero130@gmail.com>
2020-10-06 17:03:17 -0400

    fix phas2 and completed phase 3

--- public/events.js
+++ public/events.js
@@ -1,18 +1,19 @@
-window.addEventListener('DOMContentLoaded', e => {
-    fetch('/kitten/image')
-        .then(res => res.json())
-        .then(data => document.querySelector('img').src = data.src); 
-        
-    document.getElementById('new-pic').addEventListener('click', handleClick);
-});
-
 const handleClick = () => {
-    document.querySelector(".loader").innerHTML = "Loading..."
-    fetch(`/kitten/image`)
-        .then(res => res.json())
-        .then(json => {
-            document.querySelector('img').src = json.src; 
+    document.querySelector(".loader").innerHTML = "Loading...";
+    fetch("/kitten/image")
+        .then((res) => {
+            if (!res.ok) throw res;
+            return res.json();
+        })
+        .then((json) => {
+            document.querySelector("img").src = json.src;
             document.querySelector(".loader").innerHTML = "";
-        });
-}; 
-  
+        })
+        .catch((err) => alert(`Somethine went wrong! Please try again!`));
+};
+
+
+
+
+document.getElementById("new-pic").addEventListener("click", handleClick);
+window.addEventListener("DOMContentLoaded", handleClick);
