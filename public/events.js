02b18ae6755c99f1b38fd58f49999998017b7c49
Rhys Previte <previterp88@gmail.com>
2020-10-06 16:18:20 -0500

    Phase 4 complete

--- public/events.js
+++ public/events.js
@@ -12,8 +12,36 @@ const handleClick = () => {
         .catch((err) => alert(`Somethine went wrong! Please try again!`));
 };
 
+const upVote = () => {
+    fetch('/kitten/upvote', {
+        method: 'PATCH'
+    })
+        .then((res) => {
+            if (!res.ok) throw res; 
+            return res.json(); 
+        })
+        .then(data => {
+            document.querySelector(".score").innerHTML = data.score; 
+        })
+        .catch(err => console.log(err)); 
+}
 
+const downVote = () => {
+    fetch('/kitten/downvote', {
+        method: 'PATCH'
+    })
+        .then((res) => {
+            if (!res.ok) throw res; 
+            return res.json(); 
+        })
+        .then(data => {
+            document.querySelector(".score").innerHTML = data.score; 
+        })
+        .catch(err => console.log(err)); 
+}
 
 
 document.getElementById("new-pic").addEventListener("click", handleClick);
 window.addEventListener("DOMContentLoaded", handleClick);
+document.getElementById("upvote").addEventListener("click", upVote); 
+document.getElementById("downvote").addEventListener("click", downVote); 
